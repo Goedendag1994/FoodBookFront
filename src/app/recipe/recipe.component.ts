a7b0cd83d32b324ac348592e9f77efd21eb518ff
ekompanje <ekompanje@gmail.com>
2019-11-30 11:51:40 +0100

    Tenth commit, code opgeschoond

--- src/app/recipe/recipe.component.ts
+++ src/app/recipe/recipe.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpErrorResponse } from '@angular/common/http';
-// import { Router } from '@angular/router';
-import { ActivatedRoute } from '@angular/router';
 import { Recipe } from 'src/domain/recipe';
 import { RecipeService } from 'src/services/recipe.service';
 import { PartofdishService } from 'src/services/partofdish.service';
@@ -26,26 +24,6 @@ export class RecipeComponent implements OnInit {
 
   ngOnInit() {
    }
-
-   findRecipeById() {
-   console.log(this.recipeId);
-   this.recipeService.findRecipeById(this.recipeId).subscribe(
-     (recipe: Recipe) => {this.recipe = recipe; console.log(this.recipe)},
-     (error: HttpErrorResponse) => alert("Er is een fout opgetreden: " + error.status + " " + error.error + "\n" + "\nMessage:\n" + error.message),
-     () => { }
-   )
-   }
-
-   //Zoekt partofdishes m.b.v. recepiId
-   findByRecipeRecipeId() {
-    console.log(this.recipeId);
-    this.partOfDishService.findByRecipeRecipeId(this.recipeId).subscribe(
-      (partOfDishes: PartOfDish[]) => {this.partOfDishes = partOfDishes; console.log(this.partOfDishes)},
-      (error: HttpErrorResponse) => alert("Er is een fout opgetreden: " + error.status + " " + error.error + "\n" + "\nMessage:\n" + error.message),
-      () => { }
-    )
-    }
-
 
     findByRecipeTitleLike() {
       var recipeTitle = encodeURI(this.recipeTitle);
